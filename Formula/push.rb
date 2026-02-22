019a5eeb0caada328ec355492d1e485904165600
Malte Steckmeister <malte@stecki.de>
2026-02-22 21:03:24 +0100

    Update push to v0.3.0

--- Formula/push.rb
+++ Formula/push.rb
@@ -7,7 +7,7 @@ class Push < Formula
   desc "Send Pushover notifications from the command line"
   homepage "https://github.com/Stecki/push"
   url "https://github.com/Stecki/push.git",
-      tag: "v0.2.2"
+      tag: "v0.3.0"
   license "MIT"
 
   def install

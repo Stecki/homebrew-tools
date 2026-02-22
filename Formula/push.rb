72e505f6974a2af83a36d43ae02e425158a6bc95
Malte Steckmeister <malte@stecki.de>
2026-02-22 20:04:26 +0100

    Add push formula

---
+++ Formula/push.rb
@@ -0,0 +1,24 @@
+# Formula for push
+# Copy this file to your homebrew-tools tap repo:
+#   Stecki/homebrew-tools/Formula/push.rb
+# Then: brew tap Stecki/tools && brew install push
+
+class Push < Formula
+  desc "TODO: Short description of push"
+  homepage "https://github.com/Stecki/push"
+  url "https://github.com/Stecki/push.git",
+      tag: "v0.1.0"
+  license "MIT"
+
+  def install
+    # Install full tool structure under share, symlink binary
+    (share/"push").install Dir["bin", "lib", "subcommands"]
+    bin.install_symlink share/"push/bin/push"
+    bash_completion.install "completions/push.bash" => "push"
+    zsh_completion.install "completions/_push"
+  end
+
+  test do
+    assert_match "push", shell_output("#{bin}/push --version")
+  end
+end

5cee3d2c42f9b0c950e61d168f572f9d0dee49f5
Malte Steckmeister <malte@stecki.de>
2026-03-09 23:46:53 +0100

    Fix cask URL: use tap repo releases (public)

--- Casks/kompolsuche.rb
+++ Casks/kompolsuche.rb
@@ -2,7 +2,7 @@ cask "kompolsuche" do
   version "1.16"
   sha256 "35096f972dfc9f7b60747f55fdeb730ccb30722664c5b906f2277d8f1c37d5ff"
 
-  url "https://github.com/Stecki/KomPolSuche/releases/download/v#{version}/KomPolSuche.dmg"
+  url "https://github.com/Stecki/homebrew-tools/releases/download/kompolsuche-v#{version}/KomPolSuche.dmg"
   name "KomPolSuche"
   desc "Volltextsuche in kommunalpolitischen PDF-Dokumenten"
   homepage "https://github.com/Stecki/KomPolSuche"

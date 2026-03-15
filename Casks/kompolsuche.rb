01a591c65048fe8ae3be7c54c8b95f863278b78a
Malte Steckmeister <malte@stecki.de>
2026-03-16 00:15:16 +0100

    Update KomPolSuche to 1.28

--- Casks/kompolsuche.rb
+++ Casks/kompolsuche.rb
@@ -1,6 +1,6 @@
 cask "kompolsuche" do
-  version "1.27"
-  sha256 "7d50e80218b481beb3db88a20b2fb6bb1a950760d401f11149e166a74bc84e0c"
+  version "1.28"
+  sha256 "6769cf3d8f4c7a7623117a6d878d6e1cd7f044dfd17590dfb08e949a8a2635a5"
 
   url "https://github.com/Stecki/homebrew-tools/releases/download/kompolsuche-v#{version}/KomPolSuche.dmg"
   name "KomPolSuche"

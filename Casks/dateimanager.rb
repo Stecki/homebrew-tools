3fadf6e467ae25bbe544866f345d2f59ade23fdd
Malte Steckmeister <malte@stecki.de>
2026-06-07 13:18:16 +0200

    dateimanager v0.1.2

--- Casks/dateimanager.rb
+++ Casks/dateimanager.rb
@@ -1,6 +1,6 @@
 cask "dateimanager" do
-  version "0.1.1"
-  sha256 "1de434d2b31e06283afaeef57c8260a2bb320110d8983a00abc229bff530e98f"
+  version "0.1.2"
+  sha256 "4298275009e7ea7bbe1b5d938d02db8fe2c4eda535eef8b4c7cb39ce8d059201"
 
   url "https://github.com/Stecki/homebrew-tools/releases/download/dateimanager-v#{version}/Dateimanager.dmg"
   name "Dateimanager"
